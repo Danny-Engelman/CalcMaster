55615098a8be8e1fa751c9c1b83ba54212eb5ad5
Danny Engelman <danny@engelman.nl>
2015-06-18 14:01:51 +0200

    Added Console checking, fixed some bugs

--- VM365_CalcMaster.js
+++ VM365_CalcMaster.js
@@ -1,6 +1,39 @@
 /*global _spPageContextInfo */
 /*global document,console,clearTimeout,setTimeout,window,XMLHttpRequest,$ */
 /*jshint -W043*/
+
+/**
+ * Protect window.console method calls, e.g. console is not defined on IE
+ * unless dev tools are open, and IE doesn't define console.debug
+ *
+ * Chrome 41.0.2272.118: debug,error,info,log,warn,dir,dirxml,table,trace,assert,count,markTimeline,profile,profileEnd,time,timeEnd,timeStamp,timeline,timelineEnd,group,groupCollapsed,groupEnd,clear
+ * Firefox 37.0.1: log,info,warn,error,exception,debug,table,trace,dir,group,groupCollapsed,groupEnd,time,timeEnd,profile,profileEnd,assert,count
+ * Internet Explorer 11: select,log,info,warn,error,debug,assert,time,timeEnd,timeStamp,group,groupCollapsed,groupEnd,trace,clear,dir,dirxml,count,countReset,cd
+ * Safari 6.2.4: debug,error,log,info,warn,clear,dir,dirxml,table,trace,assert,count,profile,profileEnd,time,timeEnd,timeStamp,group,groupCollapsed,groupEnd
+ * Opera 28.0.1750.48: debug,error,info,log,warn,dir,dirxml,table,trace,assert,count,markTimeline,profile,profileEnd,time,timeEnd,timeStamp,timeline,timelineEnd,group,groupCollapsed,groupEnd,clear
+ */
+(function () {
+	// Union of Chrome, Firefox, IE, Opera, and Safari console methods
+	var methods = ["assert", "assert", "cd", "clear", "count", "countReset",
+    "debug", "dir", "dirxml", "dirxml", "dirxml", "error", "error", "exception",
+    "group", "group", "groupCollapsed", "groupCollapsed", "groupEnd", "info",
+    "info", "log", "log", "markTimeline", "profile", "profileEnd", "profileEnd",
+    "select", "table", "table", "time", "time", "timeEnd", "timeEnd", "timeEnd",
+    "timeEnd", "timeEnd", "timeStamp", "timeline", "timelineEnd", "trace",
+    "trace", "trace", "trace", "trace", "warn"];
+	var length = methods.length;
+	var console = (window.console = window.console || {});
+	var method;
+	var noop = function () {};
+	while (length--) {
+		method = methods[length];
+		// define undefined methods as noops to prevent errors
+		if (!console[method])
+			console[method] = noop;
+	}
+})();
+
+/* Global functions */
 var VM365_CalcMaster = { //global obect for easier debugging
 	//global settings
 	comma: _spPageContextInfo.currentLanguage === 1033 ? ',' : ';', //SharePoint function separator
@@ -13,25 +46,26 @@ var VM365_CalcMaster = { //global obect for easier debugging
 		VM365_CalcMaster.textarea = document.getElementById('onetidIODefTextValue1');
 		//adjust layout of textarea and column list
 		VM365_CalcMaster.columnList.style.height = VM365_CalcMaster.textarea.style.height = '300px';
-		VM365_CalcMaster.textarea.style.width = '600px';
+		VM365_CalcMaster.textarea.style.width = '500px';
 		//Highlight the Number datatype for HTML output
 		VM365_CalcMaster.setDOMelement('Number (outputs HTML/JavaScript)', 0, 'L_onetidTypeNumber1', 'lightGreen');
 		//add CalcMaster UI elements for Toolbar and Save Formula Reporting, remember the MIT license
 		VM365_CalcMaster.setDOMelement(
-			"<h2>CalcMaster</h2> \
-			powered by <a target=_new href=http://365coach.nl>365Coach</a> - \
-			SharePoint productivity enhancements \
-			<br>See what you can do with OOB Calculated Columns at \
-				<a href=http://viewmaster365.com/#/How>ViewMaster365.com</a> \
-			<br>Also check out the \
+			"<span style='font-size:1.3em;'>CalcMaster</span> \
+			powered by <a target=_new href=http://365coach.nl>365Coach.nl</a> \
+			<br>MIT sourcecode on <a target=_new href=https://github.com/Danny-Engelman/CalcMaster>GitHub</a> \
+			<br>\
+			<br>See <a href='http://viewmaster365.com/365coach/#/Calculated_Column_Functions_List' target=_new>SharePoint Calculated Column Functions & Syntax List</a>\
+			<br>The \
 				<a href='http://viewmaster365.com/365coach/#/Drag_Drop_Columns_in_the_EditView_Page'> \
-					Drag-Drop View Editor</a> \
-			<div style='min-height:55px;max-width:600px;'>\
+					Drag-Drop View Editor</a> is a cool bookmarklet also\
+			<div style='min-height:55px;max-width:500px;'>\
 				<img src='/" + VM365_CalcMaster.layouts + "/images/WSC16.GIF' \
 					title='Wrap Javascript code in SharePoint Formula quoted string' \
 					onclick='VM365_CalcMaster.functionConvertToICC()'> \
 				<div id='calcmasterSave'></div><div id='calcmasterHint'></div> \
 			</div>", 0, 'onetidIODefText0');
+		VM365_CalcMaster.addFunctionlist();
 		VM365_CalcMaster.setDOMelement('Update your Formula and I will check if the Formula is correct', 2);
 		//add event to the existing textarea
 		VM365_CalcMaster.textarea.onkeyup = function (event) {
@@ -55,6 +89,7 @@ var VM365_CalcMaster = { //global obect for easier debugging
 			Formula = Formula.replace(/&\"/gi, '\n  &\"'); //replace & before quote
 			Formula = Formula.replace(/\"&/gi, '"\n  &'); //replace & after quote
 			Formula = Formula.replace(/\";/gi, '\"\n;'); //replace & before quote
+			Formula = Formula.replace(/IF/g, '\nIF'); //replace & before quote
 		}
 		VM365_CalcMaster.textarea.value = Formula;
 	},
@@ -76,6 +111,9 @@ var VM365_CalcMaster = { //global obect for easier debugging
 				Formula = '="<img src=/_layouts/images/blank.gif onload=""{"&"' + Formula.replace(/\n/g, '"\n&"') + '"&"}"">"';
 			}
 			VM365_CalcMaster.setFormulaInTextarea(Formula);
+			VM365_CalcMaster.updateFormula();
+		} else {
+			VM365_CalcMaster.setDOMelement('textarea does not contain plain JavaScript', 1);
 		}
 	},
 	updateFormula: function (event) {
@@ -83,7 +121,7 @@ var VM365_CalcMaster = { //global obect for easier debugging
 		var selectedType = Array.prototype.filter.call(document.getElementsByName('ResultType'), function (r) {
 			if (r.checked) return (r);
 		});
-		document.getElementById('onetidSaveItem').style.display = 'none'; //hide OK button 
+		//document.getElementById('onetidSaveItem').style.display = 'none'; //hide OK button 
 		var Formula = VM365_CalcMaster.textarea.value;
 		VM365_CalcMaster.setFormulaInTextarea(Formula);
 		VM365_CalcMaster.updateCalculatedColumn({
@@ -100,21 +138,22 @@ var VM365_CalcMaster = { //global obect for easier debugging
 		VM365_CalcMaster.setDOMelement(calcmasterHint.join('<br>'), 3, 'calcmasterHint');
 	},
 	updateCalculatedColumn: function (column,formula,list,field) {
-		VM365_CalcMaster.setDOMelement('Updating Formula ' + column.Title + '...', 2);
+		VM365_CalcMaster.setDOMelement('Trying to save Formula ' + column.Title + '....', 2);
 		column.list = "Lists(guid'" + _spPageContextInfo.pageListId.replace(/[{}]/g, '') + "')";
 		column.field = "Fields/getbytitle('" + column.Title + "')";
-		console.info('saving',column.Title,column.Formula);
 		var xmlhttp = new XMLHttpRequest();
 		xmlhttp.onreadystatechange = function () {
+			console.info(xmlhttp.status,xmlhttp);
 			if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-				//console.info(xmlhttp.status, xmlhttp.statusText,JSON.parse(xmlhttp.responseText).error.message.value);
 				if (xmlhttp.status == 500) {
+					console.info(xmlhttp.status, xmlhttp.statusText,JSON.parse(xmlhttp.responseText).error.message.value);
 					VM365_CalcMaster.setDOMelement(JSON.parse(xmlhttp.responseText).error.message.value, 1);
 				} else {
-					VM365_CalcMaster.setDOMelement('Formula ' + column.Title + ' saved succesfully');
+					VM365_CalcMaster.setDOMelement('Formula ' + column.Title + ' saved!');
 				}
 			}
 		};
+		console.info('saving',column.Title,column.Formula);
 		xmlhttp.open("POST", _spPageContextInfo.webAbsoluteUrl + "/_api/Web/ " + column.list + "/" + column.field, true);
 		xmlhttp.setRequestHeader("X-HTTP-Method", "MERGE");
 		xmlhttp.setRequestHeader("X-RequestDigest", document.getElementById('__REQUESTDIGEST').value);
@@ -140,7 +179,7 @@ var VM365_CalcMaster = { //global obect for easier debugging
 	},
 	//leaving jQuery ajax call in here, I use it my CalcMaster Pro version
 	JQ_updateCalculatedColumn: function (column) {
-		VM365_CalcMaster.setDOMelement('Updating Formula ' + column.Title + '...', 2);
+		VM365_CalcMaster.setDOMelement('Updating Formula ' + column.Title + '... (this may take a minute)', 2);
 		column.list = "Lists(guid'" + _spPageContextInfo.pageListId.replace(/[{}]/g, '') + "')";
 		column.field = "Fields/getbytitle('" + column.Title + "')";
 		$.ajax({
@@ -174,7 +213,12 @@ var VM365_CalcMaster = { //global obect for easier debugging
 				})[column.type]
 			})
 		}); //ajax call
+	},
+	addFunctionlist:function(){
+		var F=document.getElementById('onetidIODefText0').parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.cells[1];
+		F.style.verticalAlign='top';
+		F.style.width='inherit';
+		F.innerHTML="";
 	}
-	
 };
 VM365_CalcMaster.initialize();
